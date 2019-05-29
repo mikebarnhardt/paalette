bd0c91c43571551b12dbf417f08cc58b425bc608
Mike Barnhardt <mikebarnhardt+github@gmail.com>
2019-05-29 12:04:14 -0400

    Updates

---
+++ precache-manifest.b15e89e573af3cadec38373bff64811e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ead1ebc9de298ee3326519c4fe9a0bb7",
+    "url": "/paalette/index.html"
+  },
+  {
+    "revision": "504d635ba3d8f1705996",
+    "url": "/paalette/static/css/main.def7316b.chunk.css"
+  },
+  {
+    "revision": "9136488f5760cbc73da3",
+    "url": "/paalette/static/js/2.1aa48bf7.chunk.js"
+  },
+  {
+    "revision": "504d635ba3d8f1705996",
+    "url": "/paalette/static/js/main.c6f05ab0.chunk.js"
+  },
+  {
+    "revision": "0c64d9756602e13cfbf9",
+    "url": "/paalette/static/js/runtime~main.71136c63.js"
+  }
+]);
